66793ea767fd1b461588dc5a17b0e7bf7bfb904a
Julio Cesar Montenegro <creativo@686studio.com>
2018-12-18 20:15:51 -0800

    Intro was added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App | Jelou"/>
+    <div class="o-container">
+      <div class="o-row o-flex o-flex--center">
+
+        <HelloWorld msg="Welcome to Your Vue.js App | Jelou"/>
+        <Intro />
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Intro from './components/Intro.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Intro
   }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './../node_modules/huleos.css/scss/style.scss'
 
 Vue.config.productionTip = false
 
