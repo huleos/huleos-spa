cd8be2f9fb6573a4b608d78850cb7198c0da8e90
Julio Cesar Montenegro <creativo@686studio.com>
2019-01-13 15:19:36 -0800

    Test form 2

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -20,12 +20,21 @@
           <textarea name="message" cols="30" rows="5" class="c-form-input" placeholder="Your message" required></textarea>
           <button type="submit" class="c-contact__cta">Submit</button>
         </form> -->
-        <form name="contact" netlify>
+        <form name="contact" method="POST" data-netlify="true">
   <p>
-    <label>Name <input type="text" name="name" /></label>
+    <label>Your Name: <input type="text" name="name" /></label>   
   </p>
   <p>
-    <label>Email <input type="email" name="email" /></label>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
   </p>
   <p>
     <button type="submit">Send</button>
